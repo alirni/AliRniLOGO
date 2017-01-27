8f2331a18d5d907b448495277624be4e799b96e2
Ali Ramezani <aliramezani19.1@gmail.com>
2017-01-27 15:12:31 +0330

    create logo

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <Windows.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+void main{
+	Sleep(100); std::cout << " ______  _   _    ______  ____  _  _ " << "\n";
+	Sleep(100); std::cout << "|  __  || | (_)  |  __  ||    || |(_)" << "\n";
+	Sleep(100); std::cout << "| |  | || |  _  (  (__| || || || | _ " << "\n";
+	Sleep(100); std::cout << "| |__| || | | |  | |  | || || || || |" << "\n";
+	Sleep(100); std::cout << "|  __  || | | | / /   | || || || || |" << "\n";
+	Sleep(100); std::cout << "|_|  |_||_| |_|/_/    |_||_||____||_|" << "\n";
+}
